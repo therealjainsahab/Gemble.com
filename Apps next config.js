916ad2808a2e56d715c2a4939f983da41547ce65
Sagar <princepjsagar@gmail.com>
2025-09-02 09:41:42 +0530

    Create Apps next config.js

---
+++ Apps next config.js
@@ -0,0 +1,27 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['cdn.example.com'],
+  },
+  experimental: {
+    modularizeImports: {
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+    },
+  },
+};
+
+module.exports = withPWA(nextConfig);
